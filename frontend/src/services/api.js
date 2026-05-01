17be0098ed36f85d4248c0725189bcdb0165c06e
Vaibhav Pratap <pratapvaibhav075@gmail.com>
2026-05-02 00:01:34 +0530

    connect frontend to backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://your-backend-url.up.railway.app/api"
+  baseURL: "https://team-task-manager-production-15b3.up.railway.app/api"
 });
 
 // attach token automatically
